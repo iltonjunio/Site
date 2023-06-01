4f5454a22ee8f67a52f199f30eb51877524e1181
Danielle Almeida <daniellealmeidagms@gmail.com>
2023-06-01 14:40:19 -0300

    correção (#1)

--- js/script.js
+++ js/script.js
@@ -5,10 +5,15 @@ function calcularAdmissao() {
     const hoje = new Date();
     const nasc = new Date(idade);
     let idadeconver = hoje.getFullYear() - nasc.getFullYear();
+    const mes = hoje.getMonth() - nasc.getMonth()
+    if (mes < 0 || (mes === 0 && hoje.getDate() < nasc.getDate())) {
+        idadeconver--
+    }
     let rendapercapita = renda / pessoas;
+    console.log(rendapercapita)
     let resultadoDiv = document.getElementById('resultado');
 
-    if (idadeconver >= 16 && rendapercapita <= 1.5) {
+    if (idadeconver >= 16 && rendapercapita <= 1980) {
         resultadoDiv.innerHTML = `Bem Vindo ao PSG. (0800-777-3622) Sua idade é ${idadeconver} anos.`;
         resultadoDiv.classList.remove('desfavoravel');
         resultadoDiv.classList.add('favoravel');
@@ -17,7 +22,7 @@ function calcularAdmissao() {
         resultadoDiv.innerHTML = "Você Não Tem Idade Suficiente.";
         resultadoDiv.classList.remove('favoravel', 'desfavoravel');
 
-    } else if (rendapercapita > 1.5) {
+    } else if (rendapercapita > 1980) {
         resultadoDiv.innerHTML = `Você Ultrapassou O Limite Da Renda Per Capita. Sua idade é ${idadeconver} anos.`;
         resultadoDiv.classList.remove('favoravel');
         resultadoDiv.classList.add('desfavoravel');
@@ -40,39 +45,42 @@ function calcularMedia() {
 }
 
 function calcularPresencas() {
-        let pr1 = document.querySelector('input[name="presenca1"]:checked').value
-        let pr2 = document.querySelector('input[name="presenca2"]:checked').value
-        let pr3 = document.querySelector('input[name="presenca3"]:checked').value
-        let pr4 = document.querySelector('input[name="presenca4"]:checked').value
-        let pr5 = document.querySelector('input[name="presenca5"]:checked').value
-        let pr6 = document.querySelector('input[name="presenca6"]:checked').value
-        let pr7 = document.querySelector('input[name="presenca7"]:checked').value
-        let pr8 = document.querySelector('input[name="presenca8"]:checked').value
-        let pr9 = document.querySelector('input[name="presenca9"]:checked').value
-        let pr10 = document.querySelector('input[name="presenca10"]:checked').value
-        let pr11 = document.querySelector('input[name="presenca11"]:checked').value
-        let pr12 = document.querySelector('input[name="presenca12"]:checked').value
-        let pre = (pr1 + pr2 + pr3 + pr4 + pr5 + pr6 + pr7 + pr8 + pr9 + pr10 + pr11 + pr12)
-        let prm = ((pre / 12) * 100)
-        return prm
+    let pr1 = parseInt(document.querySelector('input[name="presenca1"]:checked').value)
+    let pr2 = parseInt(document.querySelector('input[name="presenca2"]:checked').value)
+    let pr3 = parseInt(document.querySelector('input[name="presenca3"]:checked').value)
+    let pr4 = parseInt(document.querySelector('input[name="presenca4"]:checked').value)
+    let pr5 = parseInt(document.querySelector('input[name="presenca5"]:checked').value)
+    let pr6 = parseInt(document.querySelector('input[name="presenca6"]:checked').value)
+    let pr7 = parseInt(document.querySelector('input[name="presenca7"]:checked').value)
+    let pr8 = parseInt(document.querySelector('input[name="presenca8"]:checked').value)
+    let pr9 = parseInt(document.querySelector('input[name="presenca9"]:checked').value)
+    let pr10 = parseInt(document.querySelector('input[name="presenca10"]:checked').value)
+    let pr11 = parseInt(document.querySelector('input[name="presenca11"]:checked').value)
+    let pr12 = parseInt(document.querySelector('input[name="presenca12"]:checked').value)
+    let pre = (pr1 + pr2 + pr3 + pr4 + pr5 + pr6 + pr7 + pr8 + pr9 + pr10 + pr11 + pr12)
+    console.log("PRE: " + pre)
+    let prm = ((pre / 12) * 100)
+    return prm
+}
+
+function avaliarAluno() {
+    let media = parseFloat(calcularMedia());
+    let presencas = parseFloat(calcularPresencas());
+
+    console.log("Presenças: " + media)
+    console.log("Presenças: " + presencas)
+
+    resultado = document.getElementById("resultado");
+
+    if (media >= 6.0 && presencas >= 75) {
+        resultado.textContent = "Aluno Aprovado!";
+
     }
-
-    function avaliarAluno() {
-        let media = parseFloat(calcularMedia());
-        presencas = parseFloat(calcularPresencas());
-
-        resultado = document.getElementById("resultado");
-
-        if (media >= 6.0 && presencas >= 75) {
-            resultado.textContent = "Aluno Aprovado!";
-
-        } 
-        else if (media >= 5 && media < 6 && presencas >= 75){
-            resultado.textContent = "Recuperação!";
-
-        }
-        else {
-            resultado.textContent = "Aluno Reprovado!";
-        }
+    else if (media >= 5 && media < 6 && presencas >= 75) {
+        resultado.textContent = "Recuperação!";
+    }
+    else {
+        resultado.textContent = "Aluno Reprovado!";
+    }
 }
 

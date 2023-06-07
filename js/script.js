7ee06a4f1f1283e13b2b4eb7b61a5730c2be8b2c
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2023-06-07 15:37:12 -0300

    novo

--- js/script.js
+++ js/script.js
@@ -77,10 +77,282 @@ function avaliarAluno() {
 
     }
     else if (media >= 5 && media < 6 && presencas >= 75) {
-        resultado.textContent = "Recuperação!";
+        resultado.textContent = "Aluno de Recuperação!";
     }
     else {
         resultado.textContent = "Aluno Reprovado!";
     }
 }
+//-------------------------------------------
+function idade() {
+    let idade = parseInt(document.querySelector('input[name="presenca1"]:checked').value)
 
+    if (idade >= 6 && idade <= 10) {
+        console.log("Infantil!")
+        document.getElementById("resultado").textContent = "infatil: " + idade.toFixed(2);
+        return idade
+    }
+    else if (idade >= 11 && idade <= 15) {
+        console.log("Infantil juvenil!")
+        document.getElementById("resultado").textContent = "Infantil juvenil!:" + idade.toFixed(2);
+        return idade
+    }
+    else if (idade >= 16 && idade <= 21) {
+        console.log("Juvenil!")
+        document.getElementById("resultado").textContent = "Juvenil!: " + idade.toFixed(2);
+        return idade
+    }
+    else if (idade >= 22 && idade <= 35) {
+        console.log("Adulto!")
+        document.getElementById("resultado").textContent = "Adulto: " + idade.toFixed(2);
+        return idade
+    }
+    else {
+        console.log("Atleta não apto a competir!")
+        document.getElementById("resultado").textContent = "Atleta não apto a competir: " + idade.toFixed(2);
+        return idade
+    }
+}
+
+function idade1() {
+    const idade = ("Digite sua idade")
+    if (idade >= 6 && idade <= 10) {
+        console.log("Infantil!")
+        document.getElementById("resultado").textContent = "infatil: " + idade.toFixed(2);
+        return idade
+    }
+    if (idade >= 11 && idade <= 15) {
+        console.log("Infantil juvenil!")
+        document.getElementById("resultado").textContent = "Infantil juvenil!:" + idade.toFixed(2);
+        return idade
+    }
+    if (idade >= 16 && idade <= 21) {
+        console.log("Juvenil!")
+        document.getElementById("resultado").textContent = "Juvenil!: " + idade.toFixed(2);
+        return idade
+    }
+    if (idade >= 22 && idade <= 35) {
+        console.log("Adulto!")
+        document.getElementById("resultado").textContent = "Adulto: " + idade.toFixed(2);
+        return idade
+    }
+    if (idade < 6 || idade > 35) {
+        console.log("Atleta não apto a competir!")
+        document.getElementById("resultado").textContent = "Atleta não apto a competir: " + idade.toFixed(1);
+        return idade
+    }
+}
+//----------------------------------------------------------------------
+function uf() {
+    let uf = ("uf:").toUpperCase()
+    console.log(uf)
+
+    switch (uf) {
+
+        case "DF":
+            console.log("Distrito Federal*")
+            document.getElementById("resultado-").textContent = "Distrito Federal* " + uf.toFixed(1);
+            return uf
+            console.log("Brasília")
+            document.getElementById("resultado-").textContent = "Brasília " + uf.toFixed(1);
+            return uf
+            console.log("centro-oeste*")
+            document.getElementById("resultado-").textContent = "centro-oeste" + uf.toFixed(1);
+            return uf
+            break
+
+        case "AC":
+            console.log("Acre")
+            console.log("Rio Branco")
+            console.log("norte")
+            break
+
+        case "AL":
+            console.log("Alagoas")
+            console.log("Maceió")
+            console.log("nordeste")
+            break
+
+        case "AP":
+            console.log("Amapá")
+            console.log("Macapá")
+            console.log("norte")
+            break
+
+        case "AM":
+            console.log("Amazonas")
+            console.log("Manaus")
+            console.log("norte")
+            break
+
+        case "BA":
+            console.log("Bahia")
+            console.log("Salvador")
+            console.log("nordeste")
+            break
+
+        case "CE":
+            console.log("Ceará")
+            console.log("Fortaleza")
+            console.log("nordeste")
+            break
+
+        case "ES":
+            console.log("Espírito Santo")
+            console.log("Vitória")
+            console.log("sul")
+            break
+
+        case "GO":
+            console.log("Goiás")
+            console.log("Goiânia")
+            console.log("centro-oeste")
+            break
+
+        case "MA":
+            console.log("Maranhão")
+            console.log("São Luís")
+            console.log("nordeste")
+            break
+
+        case "MT":
+            console.log("Mato Grosso")
+            console.log("Cuiabá")
+            console.log("centro-oeste")
+            break
+
+        case "MS":
+            console.log("Mato Grosso Do Sul")
+            console.log("Campo Grande")
+            console.log("centro-oeste")
+            break
+
+        case "MG":
+            console.log("Minas Gerais")
+            console.log("Belo Horizonte")
+            console.log("sudeste")
+            break
+
+        case "PA":
+            console.log("Pará")
+            console.log("Belém")
+            console.log("norte")
+            break
+
+        case "PB":
+            console.log("Paraíba")
+            console.log("João Pessoa")
+            console.log("nordeste")
+            break
+
+        case "PR":
+            console.log("Paraná")
+            console.log("Curitiba")
+            console.log("sul")
+            break
+
+        case "PE":
+            console.log("Pernambuco")
+            console.log("Recife")
+            console.log("nordeste")
+            break
+
+        case "PI":
+            console.log("Piauí")
+            console.log("Teresina")
+            console.log("nordeste")
+            break
+
+        case "RJ":
+            console.log("Rio De Janeiro")
+            console.log("Rio de Janeiro")
+            console.log("sudeste")
+            break
+
+        case "RN":
+            console.log("Rio Grande Do Norte")
+            console.log("Natal")
+            console.log("nordeste")
+            break
+
+        case "RS":
+            console.log("Rio Grande Do Sul")
+            console.log("Porto Alegre")
+            console.log("sul")
+            break
+
+        case "RO":
+            console.log("Rondônia")
+            console.log("Porto Velho")
+            console.log("norte")
+            break
+
+        case "RR":
+            console.log("Roraima")
+            console.log("Boa Vista")
+            console.log("norte")
+            break
+
+        case "SC":
+            console.log("Santa Catarina")
+            console.log("Florianópolis")
+            console.log("sul")
+            break
+
+        case "SP":
+            console.log("São Paulo")
+            console.log("São Paulo")
+            console.log("sudeste")
+            break
+
+        case "SE":
+            console.log("Sergipe")
+            console.log("Aracaju")
+            console.log("nordeste")
+            break
+
+        case "TO":
+            console.log("Tocantins")
+            console.log("Palmas")
+            console.log("norte")
+            break
+
+        default:
+            console.log("Uf não encontratada")
+
+    }
+}
+//--------------------------------------
+//chat
+
+
+function casa() {
+    let name = document.getElementById('Personagem').value
+    
+    switch(name) {
+        case ('Sibila Trelawney'):
+        case ('Siblia'):
+            document.getElementById('resultado1').innerHTML = ('Boas Vindas A Corvinal')
+            break
+        case ('Cho Chang'):
+        case ('Cho'):
+            document.getElementById('resultado1').innerHTML = ('Boas Vindas A Corvinal')
+            break
+        case ('Peter Pettigrew'):
+        case ('Peter'):
+            document.getElementById('resultado1').innerHTML = ('Boas Vindas A Grifinória')
+            break
+        case ('Ninfadora Tonks'):
+        case ('Ninfadora'):
+            document.getElementById('resultado1').innerHTML = ('Boas Vindas A Lufa-Lufa')
+            break
+        case ('Narcisa Malfoy'):
+        case ('Narcisa'):
+            document.getElementById('resultado1').innerHTML = ('Boas Vindas A Sonserina')
+            break
+        default:
+            document.getElementById('resultado1').innerHTML = ('Personagem Não Encontrado!!!')
+    }
+}
+
+//-----------------------------------------------------
